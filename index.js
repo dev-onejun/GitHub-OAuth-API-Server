ab5f7260dbb47c1d9def1dad033eeda9db3f88cd
dev-onejun <wjjgjk@naver.com>
2022-06-26 07:10:33 +0900

    update index.js

--- index.js
+++ index.js
@@ -5,12 +5,43 @@ const PORT = 9081;
 const GITHUB_OAUTH_URL = 'http://github.com/login/oauth/access_token';
 
 const axios = require('axios');
-//const { Octokit, App } = require('octokit');
+const httpProxy = require('http-proxy');
 
 const pool = require('./lib/mysql_config.js');
 
-access_token = '';
+var access_token = '';
+const proxy = httpProxy.createProxyServer({});
 
+proxy.on('proxyReq', (proxyRequest, request, response) => {
+    proxyRequest.path = '/user';
+    proxyRequest.setHeader('Autorization', `token ${request.body.access_token}`);
+
+    proxyRequest.write();
+});
+proxy.on('proxyRes', (proxyResponse, request, response) => {
+    const id            = proxyResponse.data.id;
+    const avatar_url    = proxyResponse.data.avatar_url;
+    const name          = proxyResponse.data.name;
+
+    pool.query_insert_user(id, avatar_url, name, error => {
+        if(error) {
+            const response_data = {
+                id: '',
+                avatar_url: '',
+                name: '',
+            };
+            response.send(rseponse_data);
+        }
+        else {
+            const response_data = {
+                id: id,
+                avatar_url: avatar_url,
+                name: name,
+            };
+            response.send(response_data);
+        }
+    });
+});
 app.get('/githubOAuthLogin', (request, response) => {
     const client_id = '17e8286991a1ddce2954';
     const client_secret = '0f2c8b8314ab7523892519ab8db3cb3f5679c3e7';
@@ -23,44 +54,13 @@ app.get('/githubOAuthLogin', (request, response) => {
             accept: 'application/json'
         },
     }).then((res) => {
-        access_token = res.data.access_token;
-        response.redirect('/callback');
-    });
-});
+        request.body = {
+            access_token: res.data.access_token,
+        };
 
-app.get('/callback', (request, response) => {
-    axios({
-        method: 'get',
-        url: `http://api.github.com/user`,
-        headers: {
-            Authorization: 'token ' + access_token
-        },
-    }).then((res) => {
-        const id            = res.data.id;
-        const avatar_url    = res.data.avatar_url;
-        const name          = res.data.name;
-
-        console.log(response);
-
-        response.send();
-
-        /*pool.query_insert_user(id, avatar_url, name, error => {
-            if(error) {
-                const response_data = {
-                    id: '',
-                    avatar_url: '',
-                    name: '',
-                };
-                response.send(response_data);
-            }
-            else {
-                const response_data = {
-                    id: id,
-                    avatar_url: avatar_url,
-                    name: name,
-                };
-                response.send(response_data);*/
-            }
+        proxy.web(request, response, {
+            target: 'http://api.github.com',
+            selfHandleResponse: true
         });
     });
 });
